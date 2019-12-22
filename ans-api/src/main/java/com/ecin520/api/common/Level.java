f891bf9f610d0e59fde20360da2c67c1d7a3c3f2
ecin520 <875631208@qq.com>
2019-12-22 11:35:42 +0800

    更新

---
+++ ans-api/src/main/java/com/ecin520/api/common/Level.java
@@ -0,0 +1,16 @@
+package com.ecin520.api.common;
+
+/**
+ * @author: ecin520
+ * @date: 2019/12/22 9:33
+ */
+public class Level {
+
+	public static Integer getLevelByExp(Integer exp) {
+		return exp/100;
+	}
+
+	public static String getRankByExp(Integer exp) {
+
+	}
+}
